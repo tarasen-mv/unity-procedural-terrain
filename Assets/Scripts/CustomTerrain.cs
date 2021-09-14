64371ec82991d51ae58b29dd8c3a3d3730435c95
tarasen-mv <tarasen.mv@gmail.com>
2021-09-14 20:08:29 +0300

    Add random terrain button

---
+++ Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour
+{
+    public Vector2 randomHeightRange = new Vector2(0, 0.1f);
+    public Terrain terrain;
+    public TerrainData terrainData;
+
+    public void RandomTerrain()
+    {
+        var heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapResolution; y++)
+            {
+                heightMap[x, y] += UnityEngine.Random.Range(randomHeightRange.x, randomHeightRange.y);
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public static void FlattenTerrain()
+    {
+        TerrainData terrainData = Terrain.activeTerrain.terrainData;
+        var heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (int y = 0; y < terrainData.heightmapResolution; y++)
+            {
+                heightMap[x, y] = 0;
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Initialising Terrain Data");
+        terrain = this.GetComponent<Terrain>();
+        terrainData = Terrain.activeTerrain.terrainData;
+    }
+
+    private void Awake()
+    {
+        SerializedObject tagManager =
+            new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset"));
+
+        SerializedProperty tagsProp = tagManager.FindProperty(("tags"));
+
+        print(tagsProp.arraySize);
+
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        tagManager.ApplyModifiedProperties();
+
+        gameObject.tag = "Terrain";
+    }
+
+    private void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        for (int i = 0; i < tagsProp.arraySize; i++)
+        {
+            print(i);
+            print(tagsProp.arraySize);
+            print(newTag);
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            print(t.stringValue);
+            if (t.stringValue.Equals(newTag))
+            {
+                return;
+            }
+        }
+
+        tagsProp.InsertArrayElementAtIndex(0);
+        SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+        newTagProp.stringValue = newTag;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
